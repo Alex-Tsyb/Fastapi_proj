2de4e42dd7ebe0030f08e3cb4dc2d680fb3315a3
Alex-Tsyb <tsybrovsky@gmail.com>
2024-04-29 19:11:45 +0300

    apdate database

--- src/database/db.py
+++ src/database/db.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///fastapi.sqlite'
+
+# docker run --name fastrestproj-postgres -p 5538:5432 -e POSTGRES_PASSWORD=changeme -d postgres
+# docker ps
+# docker exec -it fastrestproj-postgres bash
+# psql -h localhost -U postgres
+# CREATE DATABASE fast_rest_proj; 
+# DROP DATABASE fast_rest_proj; 
+# \l    - list of databases
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:changeme@localhost:5538/fast_rest_proj"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
